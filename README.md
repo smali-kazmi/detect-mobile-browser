51f4a6c72f9f62400f4c37e6c7df2fc6fdae9104
SM@K <smali.kazmi@hotmail.com>
2014-05-01 00:54:09 +0500

    Add Browse Detector JS

--- README.md
+++ README.md
@@ -2,3 +2,15 @@ detect-mobile-browser
 =====================
 
 Small plugin to detect mobile browser 
+
+**Usage:-**
+
+```javascript
+alert(SmartPhone.isAndriod());
+```
+
+OR
+
+```javascript
+alert(SmartPhone.isAny());
+```
---
+++ detect-browser.js
@@ -0,0 +1,43 @@
+/**
+ * 
+ * @auther SM@K<smali.kazmi@hotmail.com>
+ * @description website: smak.pk
+ */
+
+var SmartPhone = {
+    getUserAgent: function() {
+        return navigator.userAgent;
+    },
+    isAndroid: function() {
+        return this.getUserAgent().match(/Android/i);
+    },
+    isBlackBerry: function() {
+        return this.getUserAgent().match(/BlackBerry/i);
+    },
+    isIOS: function() {
+        return this.getUserAgent().match(/iPhone|iPad|iPod/i);
+    },
+    isOpera: function() {
+        return this.getUserAgent().match(/Opera Mini/i);
+    },
+    isWindows: function() {
+        return this.getUserAgent().match(/IEMobile/i);
+    },
+    isAny: function() {
+        var foundAny = false;
+        var getAllMethods = Object.getOwnPropertyNames(SmartPhone).filter(function(property) {
+            return typeof SmartPhone[property] == 'function';
+        });
+
+        for (var index in getAllMethods) {
+            if (getAllMethods[index] === 'getUserAgent' || getAllMethods[index] === 'isAny') {
+                continue;
+            }
+            if (SmartPhone[getAllMethods[index]]()) {
+                foundAny = true;
+                break;
+            }
+        }
+        return foundAny;
+    }
+};
