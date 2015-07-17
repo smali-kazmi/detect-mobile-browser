b07d6e5256886dd95f1dd41c50b41cdc0af946f8
SM@K <smali.kazmi@hotmail.com>
2015-07-17 16:40:40 +0500

    adding user agents continue...

--- test/userAgents.js
+++ test/userAgents.js
@@ -1,19 +1,23 @@
 module.exports = {
 	'andriod': {
 		'call': 'isAndroid',
-		'userAgent': ''
+		'userAgent': 'Mozilla/5.0 (Linux; U; Android 4.3; en-us; SM-N900T Build/JSS15J) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30'
 	},
 	'blackBerry': {
 		'call': 'isBlackBerry',
-		'userAgent': ''
+		'userAgent': 'Mozilla/5.0 (BlackBerry; U; RIM Tablet OS 2.1.0; en-US) AppleWebKit/536.2+ (KHTML like Gecko) Version/7.2.1.0 Safari/536.2+'
+	},
+	'blackBerryPlayBook': {
+		'call': 'isBlackBerryPlayBook',
+		'userAgent': 'Mozilla/5.0 (PlayBook; U; RIM Tablet OS 2.1.0; en-US) AppleWebKit/536.2+ (KHTML like Gecko) Version/7.2.1.0 Safari/536.2+'
 	},	
 	'blackBerry10': {
 		'call': 'isBlackBerry10',
-		'userAgent': ''
+		'userAgent': 'Mozilla/5.0 (BB10; Touch) AppleWebKit/537.10+ (KHTML, like Gecko) Version/10.0.9.2372 Mobile Safari/537.10+'
 	},	
 	'iphone': {
 		'call': 'isIPhone',
-		'userAgent': ''
+		'userAgent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 7_0 like Mac OS X; en-us) AppleWebKit/537.51.1 (KHTML, like Gecko) Version/7.0 Mobile/11A465 Safari/9537.53'
 	},
 	'ipad': {
 		'call': 'isIPad',
