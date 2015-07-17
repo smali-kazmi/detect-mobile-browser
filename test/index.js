f1b437cc11c9704b191d94e2369eb6d6cb66a056
SM@K <smali.kazmi@hotmail.com>
2015-07-17 17:47:25 +0500

    working on unit test

--- test/index.js
+++ test/index.js
@@ -1,8 +1,25 @@
 var assert = require("assert");
-var SmartPhone = require('../detect-brwoser');
+var userAgents = require('./userAgents');
 
+function evaluateUserAgent(ua) {
+
+}
 
 describe('SmartPhone', function() {
 
+	describe('check all SmartPhone function as per userAgents', function(){
 
+		for(index in userAgents) {
+			var ua = userAgents[index];
+			
+			it('it should check: ' + index, function(){
+				var SmartPhone = require('../detect-browser')(false);
+				SmartPhone.setUserAgent(ua['userAgent'])
+				console.log(ua['call']);
+				assert.notEqual(null, 1);
+			});	
+
+		}
+
+	});
 });
