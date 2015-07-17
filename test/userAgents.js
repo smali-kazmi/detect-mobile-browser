80efd121fd466257fb605550035d3bca25967d18
SM@K <smali.kazmi@hotmail.com>
2015-07-17 18:04:22 +0500

    refactor userAgents list

--- test/userAgents.js
+++ test/userAgents.js
@@ -1,58 +1,58 @@
-module.exports = {
-	'andriod': {
+module.exports = [
+	{
 		'call': 'isAndroid',
 		'userAgent': 'Mozilla/5.0 (Linux; U; Android 4.3; en-us; SM-N900T Build/JSS15J) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30'
 	},
-	'blackBerry': {
+	{
 		'call': 'isBlackBerry',
 		'userAgent': 'Mozilla/5.0 (BlackBerry; U; RIM Tablet OS 2.1.0; en-US) AppleWebKit/536.2+ (KHTML like Gecko) Version/7.2.1.0 Safari/536.2+'
 	},
-	'blackBerryPlayBook': {
+	{
 		'call': 'isBlackBerryPlayBook',
 		'userAgent': 'Mozilla/5.0 (PlayBook; U; RIM Tablet OS 2.1.0; en-US) AppleWebKit/536.2+ (KHTML like Gecko) Version/7.2.1.0 Safari/536.2+'
 	},	
-	'blackBerry10': {
+	{
 		'call': 'isBlackBerry10',
 		'userAgent': 'Mozilla/5.0 (BB10; Touch) AppleWebKit/537.10+ (KHTML, like Gecko) Version/10.0.9.2372 Mobile Safari/537.10+'
 	},	
-	'iphone': {
+	{
 		'call': 'isIPhone',
 		'userAgent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 7_0 like Mac OS X; en-us) AppleWebKit/537.51.1 (KHTML, like Gecko) Version/7.0 Mobile/11A465 Safari/9537.53'
 	},
-	'ipad': {
+	{
 		'call': 'isIPad',
 		'userAgent': 'Mozilla/5.0 (iPad; CPU OS 7_0 like Mac OS X) AppleWebKit/537.51.1 (KHTML, like Gecko) Version/7.0 Mobile/11A465 Safari/9537.53'
 	},
-	'ipod': {
+	{
 		'call': 'isIPod',
 		'userAgent': 'Mozilla/5.0 (iPod; U; CPU like Mac OS X; en) AppleWebKit/420.1 (KHTML, like Gecko) Version/3.0 Mobile/3A101a Safari/419.3'
 	},
-	'opera': {
+	{
 		'call': 'isOpera',
 		'userAgent': 'Opera/9.80 (J2ME/MIDP; Opera Mini/9.80 (S60; SymbOS; Opera Mobi/23.348; U; en) Presto/2.5.25 Version/10.54'
 	},
-	'windowMobile': {
+	{
 		'call': 'isWindowsMobile',
 		'userAgent': 'Mozilla/5.0 (compatible; MSIE 9.0; Windows Phone OS 7.5; Trident/5.0; IEMobile/9.0'
 	},
-	'windowDesktop': {
+	{
 		'call': 'isWindowsDesktop',
 		'userAgent': 'Mozilla/5.0 (compatible; MSIE 9.0; Windows Phone OS 7.5; Trident/5.0; WPDesktop/9.0'
 	},
-	'firefox': {
+	{
 		'call': 'isFireFox',
 		'userAgent': 'Mozilla/5.0 (Mobile; rv:14.0) Gecko/14.0 Firefox/14.0'
 	},
-	'nexus': {
+	{
 		'call': 'isNexus',
 		'userAgent': 'Mozilla/5.0 (Linux; Android 4.4.4; en-us; Nexus 4 Build/JOP40D) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2307.2 Mobile Safari/537.36'
 	},
-	'kindleFire': {
+	{
 		'call': 'isKindleFire',
 		'userAgent': 'Mozilla/5.0 (Linux; U; Android android-version; locale; KFOT Build/product-build) AppleWebKit/webkit-version (KHTML, like Gecko) Silk/browser-version like Chrome/chrome-version Safari/webkit-version'
 	},
-	'palm': {
+	{
 		'call': 'isPalm',
-		'userAgent': 'PalmSource Palm'
+		'userAgent': 'Mozilla/4.0 (compatible; MSIE 6.0; Windows 95; PalmSource; Blazer 3.0) 16; 160x160'
 	}
-};
+];
