7e484dfe0a6c8b3297b7d93606a75869a9905ee4
SM@K <smali.kazmi@hotmail.com>
2015-07-17 16:36:45 +0500

    add userAgent template for unit testing

---
+++ test/userAgents.js
@@ -0,0 +1,62 @@
+module.exports = {
+	'andriod': {
+		'call': 'isAndroid',
+		'userAgent': ''
+	},
+	'blackBerry': {
+		'call': 'isBlackBerry',
+		'userAgent': ''
+	},	
+	'blackBerry10': {
+		'call': 'isBlackBerry10',
+		'userAgent': ''
+	},	
+	'iphone': {
+		'call': 'isIPhone',
+		'userAgent': ''
+	},
+	'ipad': {
+		'call': 'isIPad',
+		'userAgent': ''
+	},
+	'ipod': {
+		'call': 'isIPod',
+		'userAgent': ''
+	},
+	'opera': {
+		'call': 'isOpera',
+		'userAgent': ''
+	},
+	'window': {
+		'call': 'isWindows',
+		'userAgent': ''
+	},
+	'windowMobile': {
+		'call': 'isWindowsMobile',
+		'userAgent': ''
+	},
+	'windowDesktop': {
+		'call': 'isWindowsDesktop',
+		'userAgent': ''
+	},
+	'firefox': {
+		'call': 'isFireFox',
+		'userAgent': ''
+	},
+	'nexus': {
+		'call': 'isNexus',
+		'userAgent': ''
+	},
+	'kindleFire': {
+		'call': 'isKindleFire',
+		'userAgent': ''
+	},
+	'palm': {
+		'call': 'isPalm',
+		'userAgent': ''
+	},
+	'palm': {
+		'call': 'isPalm',
+		'userAgent': ''
+	},
+};
